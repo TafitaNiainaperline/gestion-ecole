9bf57c9ce23a2eec4c90412e6a8dc8620d3f675a
Tafitaniaina <tafitaperl@gmail.com>
2025-12-09 13:50:37 +0300

    Update app.module.ts to import all new modules
    - Import StudentModule
    - Import ParentModule
    - Import NotificationModule
    - Import SmsLogModule
    - All modules are now registered and available

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { SmsModule } from './sms/sms.module';
+import { StudentModule } from './student/student.module';
+import { ParentModule } from './parent/parent.module';
+import { NotificationModule } from './notification/notification.module';
+import { SmsLogModule } from './sms-log/sms-log.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_MONGO_URI || 'mongodb://localhost:27017/ecole'),
     AuthModule,
     SmsModule,
+    StudentModule,
+    ParentModule,
+    NotificationModule,
+    SmsLogModule,
   ],
 })
 export class AppModule {}
