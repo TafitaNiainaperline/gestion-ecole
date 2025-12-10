d828e4c655a4bb0228b1d84e0b276bc889775e0c
Tafitaniaina <tafitaperl@gmail.com>
2025-12-10 11:42:25 +0300

    Update UserController with @ApiBearerAuth() decorator

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,30 +1,31 @@
 import { Controller, Get, Patch, Delete, Param, Body } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
-import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../commons/decorators/roles.decorator';
+import { Role } from '../../commons/enums/role.enum';
 
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
+@Roles(Role.ADMIN, Role.TEACHER)
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Public()
   @Get()
   async getAllUsers() {
     return this.userService.getAllUsers();
   }
 
-  @Public()
   @Get(':id')
   async getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id);
   }
 
-  @Public()
   @Patch(':id')
   async updateUser(@Param('id') id: string, @Body() updateData: any) {
     return this.userService.updateUser(id, updateData);
   }
 
-  @Public()
   @Delete(':id')
   async deleteUser(@Param('id') id: string) {
     return this.userService.deleteUser(id);
