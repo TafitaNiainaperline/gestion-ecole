916f883d08551611ed7dbd7bc9198e2f79de7543
Tafitaniaina <tafitaperl@gmail.com>
2025-12-10 11:44:31 +0300

    Remove UserRole enum and update exports

--- src/commons/enums/index.ts
+++ src/commons/enums/index.ts
@@ -2,5 +2,4 @@ export * from './student.enum';
 export * from './parent.enum';
 export * from './notification.enum';
 export * from './sms-log.enum';
-export * from './user.enum';
 export * from './role.enum';
