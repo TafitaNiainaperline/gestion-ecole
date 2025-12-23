428d1b3938573ea25084a91c7c3b39eb2885c1fb
amigoskazz <kaznarahandrinarivo@gmail.com>
2025-12-23 11:35:22 +0300

    Remove SMS WebSocket gateway, webhook controller, and related services, transitioning to a simplified module structure.

--- src/modules/sms/sms.module.ts
+++ src/modules/sms/sms.module.ts
@@ -1,15 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { SmsService } from './sms.service';
 import { SmsController } from './sms.controller';
-import { SmsGateway } from './gateways/sms.gateway';
-import { SmsSocketClientService } from './sms-socket-client.service';
-import { SmsWebhookController } from './sms-webhook.controller';
 import { SmsLogModule } from '../sms-log/sms-log.module';
 
 @Module({
   imports: [forwardRef(() => SmsLogModule)],
-  controllers: [SmsController, SmsWebhookController],
-  providers: [SmsService, SmsGateway, SmsSocketClientService],
-  exports: [SmsService, SmsGateway],
+  controllers: [SmsController],
+  providers: [SmsService],
+  exports: [SmsService],
 })
 export class SmsModule {}
